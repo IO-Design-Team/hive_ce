4eb0dbc7807e7210831a2c71f056ac15c9e32e52
Rexios <rexios@rexios.dev>
2024-05-29 11:21:50 -0400

    Convert backend_manager.dart

--- hive/lib/src/backend/js/native/backend_manager.dart
+++ hive/lib/src/backend/js/native/backend_manager.dart
@@ -1,16 +1,13 @@
 import 'dart:async';
-import 'dart:html';
-import 'dart:indexed_db';
-import 'dart:js' as js;
+import 'dart:js_interop';
 import 'package:hive/hive.dart';
 import 'package:hive/src/backend/js/native/storage_backend_js.dart';
 import 'package:hive/src/backend/storage_backend.dart';
+import 'package:web/web.dart';
 
 /// Opens IndexedDB databases
 class BackendManager implements BackendManagerInterface {
-  IdbFactory? get indexedDB => js.context.hasProperty('window')
-      ? window.indexedDB
-      : WorkerGlobalScope.instance.indexedDB;
+  IDBFactory? get indexedDB => window.self.indexedDB;
 
   @override
   Future<StorageBackend> open(String name, String? path, bool crashRecovery,
@@ -19,25 +16,27 @@ class BackendManager implements BackendManagerInterface {
     final databaseName = collection ?? name;
     final objectStoreName = collection == null ? 'box' : name;
 
-    var db =
-        await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
-      var db = e.target.result as Database;
-      if (!(db.objectStoreNames ?? []).contains(objectStoreName)) {
-        db.createObjectStore(objectStoreName);
-      }
-    });
+    var db = await openDatabase(
+      databaseName,
+      onUpgradeNeeded: (e) {
+        var db = e.target.result as IDBDatabase;
+        if (!db.objectStoreNames.contains(objectStoreName)) {
+          db.createObjectStore(objectStoreName);
+        }
+      },
+    );
 
     // in case the objectStore is not contained, re-open the db and
     // update version
-    if (!(db.objectStoreNames ?? []).contains(objectStoreName)) {
+    if (!db.objectStoreNames.contains(objectStoreName)) {
       print(
           'Creating objectStore $objectStoreName in database $databaseName...');
-      db = await indexedDB!.open(
+      db = await openDatabase(
         databaseName,
-        version: (db.version ?? 1) + 1,
+        version: db.version + 1,
         onUpgradeNeeded: (e) {
-          var db = e.target.result as Database;
-          if (!(db.objectStoreNames ?? []).contains(objectStoreName)) {
+          var db = e.target.result as IDBDatabase;
+          if (!db.objectStoreNames.contains(objectStoreName)) {
             db.createObjectStore(objectStoreName);
           }
         },
@@ -59,17 +58,16 @@ class BackendManager implements BackendManagerInterface {
 
     // directly deleting the entire DB if a non-collection Box
     if (collection == null) {
-      await indexedDB!.deleteDatabase(databaseName);
+      await deleteDatabase(databaseName);
     } else {
-      final db =
-          await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
-        var db = e.target.result as Database;
-        if ((db.objectStoreNames ?? []).contains(objectStoreName)) {
+      final db = await openDatabase(databaseName, onUpgradeNeeded: (e) {
+        var db = e.target.result as IDBDatabase;
+        if (db.objectStoreNames.contains(objectStoreName)) {
           db.deleteObjectStore(objectStoreName);
         }
       });
-      if ((db.objectStoreNames ?? []).isEmpty) {
-        indexedDB!.deleteDatabase(databaseName);
+      if (db.objectStoreNames.length == 0) {
+        await deleteDatabase(databaseName);
       }
     }
   }
@@ -83,21 +81,45 @@ class BackendManager implements BackendManagerInterface {
     try {
       var _exists = true;
       if (collection == null) {
-        await indexedDB!.open(databaseName, version: 1, onUpgradeNeeded: (e) {
+        await openDatabase(databaseName, onUpgradeNeeded: (e) {
           e.target.transaction!.abort();
           _exists = false;
         });
       } else {
-        final db =
-            await indexedDB!.open(collection, version: 1, onUpgradeNeeded: (e) {
-          var db = e.target.result as Database;
-          _exists = (db.objectStoreNames ?? []).contains(objectStoreName);
+        final db = await openDatabase(collection, onUpgradeNeeded: (e) {
+          var db = e.target.result as IDBDatabase;
+          _exists = db.objectStoreNames.contains(objectStoreName);
         });
-        _exists = (db.objectStoreNames ?? []).contains(objectStoreName);
+        _exists = db.objectStoreNames.contains(objectStoreName);
       }
       return _exists;
     } catch (error) {
       return false;
     }
+  }
+
+  Future<IDBDatabase> openDatabase(
+    String name, {
+    int version = 1,
+    void Function(dynamic event)? onUpgradeNeeded,
+  }) async {
+    final request = indexedDB!.open(name, version);
+    request.onupgradeneeded = onUpgradeNeeded?.toJS;
+
+    final completer = Completer<IDBDatabase>();
+    request.onsuccess = (e) {
+      completer.complete(e.target.result as IDBDatabase);
+    }.toJS;
+    return completer.future;
+  }
+
+  Future<void> deleteDatabase(String name) async {
+    final request = indexedDB!.deleteDatabase(name);
+    final completer = Completer();
+    request.onsuccess = (e) {
+      completer.complete();
+    }.toJS;
+    request.onerror = completer.completeError.toJS;
+    return completer.future;
   }
 }
