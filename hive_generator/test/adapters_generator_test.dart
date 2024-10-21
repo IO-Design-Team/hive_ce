bd9ce0e60cd71cc65b638c9db9427d771e504b30
Rexios <rexios@rexios.dev>
2024-10-21 12:20:09 -0400

    More tests

--- hive_generator/test/adapters_generator_test.dart
+++ hive_generator/test/adapters_generator_test.dart
@@ -3,29 +3,17 @@ import 'package:test/test.dart';
 
 import 'test_utils.dart';
 
-void main() {
-  group('adapters_generator', () {
-    test('clean', () async {
-      expectGeneration(
-        input: {
-          'pubspec.yaml': pubspec,
-          'lib/hive/hive_adapters.dart': '''
+const directives = '''
 import 'package:hive_ce/hive.dart';
 part 'hive_adapters.g.dart';
+''';
 
-@GenerateAdapters([AdapterSpec<Person>()])
-class Person {
-  const Person({required this.name, required this.age});
+const commonOutputs = {
+  'lib/hive/hive_adapters.g.dart': fileExists,
+  'lib/hive/hive_registrar.g.dart': fileExists,
+};
 
-  final String name;
-  final int age;
-}
-''',
-        },
-        output: {
-          'lib/hive/hive_adapters.g.dart': fileExists,
-          'lib/hive/hive_registrar.g.dart': fileExists,
-          'lib/hive/hive_schema.yaml': '''
+const personSchema = '''
 $schemaComment
 nextTypeId: 1
 types: 
@@ -37,22 +25,50 @@ types:
         index: 0
       age: 
         index: 1
-''',
-        },
-      );
-    });
+''';
 
-    test('add type', () async {
-      // Adding Person2 while removing Person should result in Person2 having a
-      // typeId of 1
+void main() {
+  group('adapters_generator', () {
+    test('fresh', () {
       expectGeneration(
         input: {
           'pubspec.yaml': pubspec,
           'lib/hive/hive_adapters.dart': '''
-import 'package:hive_ce/hive.dart';
-part 'hive_adapters.g.dart';
+$directives
 
-@GenerateAdapters([AdapterSpec<Person2>()])
+@GenerateAdapters([AdapterSpec<Person>()])
+class Person {
+  const Person({required this.name, required this.age});
+
+  final String name;
+  final int age;
+}
+''',
+        },
+        output: {
+          ...commonOutputs,
+          'lib/hive/hive_schema.yaml': personSchema,
+        },
+      );
+    });
+
+    test('add type', () {
+      // Adding Person2 should result in Person2 having a typeId of 1 no matter
+      // the order in the annotation
+      expectGeneration(
+        input: {
+          'pubspec.yaml': pubspec,
+          'lib/hive/hive_adapters.dart': '''
+$directives
+
+@GenerateAdapters([AdapterSpec<Person2>(), AdapterSpec<Person>()])
+class Person {
+  const Person({required this.name, required this.age});
+
+  final String name;
+  final int age;
+}
+
 class Person2 {
   const Person2({required this.name, required this.age});
 
@@ -60,8 +76,13 @@ class Person2 {
   final int age;
 }
 ''',
+          'lib/hive/hive_schema.yaml': personSchema,
+        },
+        output: {
+          ...commonOutputs,
           'lib/hive/hive_schema.yaml': '''
-nextTypeId: 1
+$schemaComment
+nextTypeId: 2
 types: 
   Person: 
     typeId: 0
@@ -71,11 +92,40 @@ types:
         index: 0
       age: 
         index: 1
+  Person2: 
+    typeId: 1
+    nextIndex: 2
+    fields: 
+      name: 
+        index: 0
+      age: 
+        index: 1
 ''',
         },
+      );
+    });
+
+    test('add and remove type', () {
+      // Adding Person2 while removing Person should result in Person2 having a
+      // typeId of 1
+      expectGeneration(
+        input: {
+          'pubspec.yaml': pubspec,
+          'lib/hive/hive_adapters.dart': '''
+$directives
+
+@GenerateAdapters([AdapterSpec<Person2>()])
+class Person2 {
+  const Person2({required this.name, required this.age});
+
+  final String name;
+  final int age;
+}
+''',
+          'lib/hive/hive_schema.yaml': personSchema,
+        },
         output: {
-          'lib/hive/hive_adapters.g.dart': fileExists,
-          'lib/hive/hive_registrar.g.dart': fileExists,
+          ...commonOutputs,
           'lib/hive/hive_schema.yaml': '''
 $schemaComment
 nextTypeId: 2
@@ -88,6 +138,82 @@ types:
         index: 0
       age: 
         index: 1
+''',
+        },
+      );
+    });
+
+    test('add field', () {
+      // A new field on Person should have the last index no matter the order
+      expectGeneration(
+        input: {
+          'pubspec.yaml': pubspec,
+          'lib/hive/hive_adapters.dart': '''
+$directives
+
+@GenerateAdapters([AdapterSpec<Person>()])
+class Person {
+  const Person({required this.balance, required this.name, required this.age});
+
+  final double balance;
+  final String name;
+  final int age;
+}
+''',
+          'lib/hive/hive_schema.yaml': personSchema,
+        },
+        output: {
+          ...commonOutputs,
+          'lib/hive/hive_schema.yaml': '''
+$schemaComment
+nextTypeId: 1
+types: 
+  Person: 
+    typeId: 0
+    nextIndex: 3
+    fields: 
+      name: 
+        index: 0
+      age: 
+        index: 1
+      balance: 
+        index: 2
+''',
+        },
+      );
+    });
+
+    test('replace field', () {
+      expectGeneration(
+        input: {
+          'pubspec.yaml': pubspec,
+          'lib/hive/hive_adapters.dart': '''
+$directives
+
+@GenerateAdapters([AdapterSpec<Person>()])
+class Person {
+  const Person({required this.name, required this.balance});
+
+  final String name;
+  final double balance;
+}
+''',
+          'lib/hive/hive_schema.yaml': personSchema,
+        },
+        output: {
+          ...commonOutputs,
+          'lib/hive/hive_schema.yaml': '''
+$schemaComment
+nextTypeId: 1
+types: 
+  Person: 
+    typeId: 0
+    nextIndex: 3
+    fields: 
+      name: 
+        index: 0
+      balance: 
+        index: 2
 ''',
         },
       );
