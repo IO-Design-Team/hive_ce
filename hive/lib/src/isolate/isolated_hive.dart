630e1afbbe1e87e9b305d249c4d58ea18a7f73d8
Rexios <rexios@rexios.dev>
2025-03-08 16:17:52 -0500

    wip

--- hive/lib/src/isolate/isolated_hive.dart
+++ hive/lib/src/isolate/isolated_hive.dart
@@ -5,6 +5,7 @@ import 'package:hive_ce/hive.dart';
 import 'package:hive_ce/src/box/default_compaction_strategy.dart';
 import 'package:hive_ce/src/box/default_key_comparator.dart';
 import 'package:isolate_channel/isolate_channel.dart';
+import 'package:meta/meta.dart';
 
 class IsolatedHive {
   late final IsolateMethodChannel _hiveChannel;
@@ -33,8 +34,8 @@ class IsolatedHive {
   Future<IsolatedBox<E>> openBox<E>(
     String name, {
     HiveCipher? encryptionCipher,
-    KeyComparator keyComparator = defaultKeyComparator,
-    CompactionStrategy compactionStrategy = defaultCompactionStrategy,
+    KeyComparator? keyComparator,
+    CompactionStrategy? compactionStrategy,
     bool crashRecovery = true,
     String? path,
     Uint8List? bytes,
@@ -61,8 +62,8 @@ class IsolatedHive {
   Future<IsolatedBox<E>> openLazyBox<E>(
     String name, {
     HiveCipher? encryptionCipher,
-    KeyComparator keyComparator = defaultKeyComparator,
-    CompactionStrategy compactionStrategy = defaultCompactionStrategy,
+    KeyComparator? keyComparator,
+    CompactionStrategy? compactionStrategy,
     bool crashRecovery = true,
     String? path,
     String? collection,
@@ -103,8 +104,6 @@ class IsolatedHive {
   Future<bool> boxExists(String name, {String? path}) =>
       _hiveChannel.invokeMethod('boxExists', {'name': name, 'path': path});
 
-  Future<void> resetAdapters() => _hiveChannel.invokeMethod('resetAdapters');
-
   void registerAdapter<T>(
     TypeAdapter<T> adapter, {
     bool internal = false,
@@ -118,6 +117,9 @@ class IsolatedHive {
 
   Future<bool> isAdapterRegistered(int typeId) =>
       _hiveChannel.invokeMethod('isAdapterRegistered', typeId);
+
+  @visibleForTesting
+  Future<void> resetAdapters() => _hiveChannel.invokeMethod('resetAdapters');
 
   Future<void> ignoreTypeId<T>(int typeId) =>
       _hiveChannel.invokeMethod('ignoreTypeId', typeId);
@@ -138,7 +140,65 @@ void _handleMethodCall(IsolateMethodCall call, IsolateResult result) async {
       Hive.init(call.arguments);
       result(null);
     case 'openBox':
-      await Hive.openBox(call.arguments);
+      await Hive.openBox(
+        call.arguments['name'],
+        encryptionCipher: call.arguments['encryptionCipher'],
+        keyComparator: call.arguments['keyComparator'] ?? defaultKeyComparator,
+        compactionStrategy:
+            call.arguments['compactionStrategy'] ?? defaultCompactionStrategy,
+        crashRecovery: call.arguments['crashRecovery'],
+        path: call.arguments['path'],
+        bytes: call.arguments['bytes'],
+        collection: call.arguments['collection'],
+      );
+      result(null);
+    case 'openLazyBox':
+      await Hive.openLazyBox(
+        call.arguments['name'],
+        encryptionCipher: call.arguments['encryptionCipher'],
+        keyComparator: call.arguments['keyComparator'] ?? defaultKeyComparator,
+        compactionStrategy:
+            call.arguments['compactionStrategy'] ?? defaultCompactionStrategy,
+        crashRecovery: call.arguments['crashRecovery'],
+        path: call.arguments['path'],
+        collection: call.arguments['collection'],
+      );
+      result(null);
+    case 'isBoxOpen':
+      result(Hive.isBoxOpen(call.arguments));
+    case 'close':
+      await Hive.close();
+      result(null);
+    case 'deleteBoxFromDisk':
+      await Hive.deleteBoxFromDisk(
+        call.arguments['name'],
+        path: call.arguments['path'],
+      );
+      result(null);
+    case 'deleteFromDisk':
+      await Hive.deleteFromDisk();
+      result(null);
+    case 'boxExists':
+      result(
+        Hive.boxExists(call.arguments['name'], path: call.arguments['path']),
+      );
+      result(null);
+    case 'registerAdapter':
+      Hive.registerAdapter(
+        call.arguments['adapter'],
+        internal: call.arguments['internal'],
+        override: call.arguments['override'],
+      );
+      result(null);
+    case 'isAdapterRegistered':
+      result(Hive.isAdapterRegistered(call.arguments));
+    case 'resetAdapters':
+      // This is a proxy
+      // ignore: invalid_use_of_visible_for_testing_member
+      Hive.resetAdapters();
+      result(null);
+    case 'ignoreTypeId':
+      Hive.ignoreTypeId(call.arguments);
       result(null);
     default:
       throw UnimplementedError();
