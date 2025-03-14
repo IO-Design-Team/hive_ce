868468ffdda2e8bf861f048b6f29c70369775052
Rexios <rexios@rexios.dev>
2025-03-14 13:43:29 -0400

    ...

--- hive/test/integration/isolate_test.dart
+++ hive/test/integration/isolate_test.dart
@@ -113,7 +113,7 @@ void main() async {
             ]),
             completes,
           );
-          final box = hive.box<int>('test');
+          final box = await hive.openBox<int>('test');
           expect(await box.length, 10000);
         });
       });
