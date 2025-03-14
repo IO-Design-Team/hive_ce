11bc1c46dfe1cb866a4bea4ccc7d157c44bc2e8f
Rexios <rexios@rexios.dev>
2025-03-14 14:02:56 -0400

    ...

--- hive_generator/example/lib/hive/hive_registrar.g.dart
+++ hive_generator/example/lib/hive/hive_registrar.g.dart
@@ -25,19 +25,17 @@ extension HiveRegistrar on HiveInterface {
 }
 
 extension IsolatedHiveRegistrar on IsolatedHiveInterface {
-  Future<void> registerAdapters() {
-    return Future.wait([
-      registerAdapter(Class1Adapter()),
-      registerAdapter(Class2Adapter()),
-      registerAdapter(ClassSpec1Adapter()),
-      registerAdapter(ClassSpec2Adapter()),
-      registerAdapter(ConstructorDefaultsAdapter()),
-      registerAdapter(EmptyClassAdapter()),
-      registerAdapter(Enum1Adapter()),
-      registerAdapter(EnumSpecAdapter()),
-      registerAdapter(IterableClassAdapter()),
-      registerAdapter(NamedImportsAdapter()),
-      registerAdapter(NullableTypesAdapter()),
-    ]);
+  void registerAdapters() {
+    registerAdapter(Class1Adapter());
+    registerAdapter(Class2Adapter());
+    registerAdapter(ClassSpec1Adapter());
+    registerAdapter(ClassSpec2Adapter());
+    registerAdapter(ConstructorDefaultsAdapter());
+    registerAdapter(EmptyClassAdapter());
+    registerAdapter(Enum1Adapter());
+    registerAdapter(EnumSpecAdapter());
+    registerAdapter(IterableClassAdapter());
+    registerAdapter(NamedImportsAdapter());
+    registerAdapter(NullableTypesAdapter());
   }
 }
