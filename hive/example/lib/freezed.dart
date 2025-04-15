ce7c1d5c43b693adde79c9a44ef779b98e9884aa
Rexios <rexios@rexios.dev>
2025-04-15 12:48:36 -0400

    Upgrade freezed

--- hive/example/lib/freezed.dart
+++ hive/example/lib/freezed.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'freezed.freezed.dart';
 
 @freezed
-class FreezedPerson with _$FreezedPerson {
+sealed class FreezedPerson with _$FreezedPerson {
   const factory FreezedPerson({
     required String firstName,
     @Default('') String middleName,
