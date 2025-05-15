78004a928b2474882c1ae7809fd411edfdae793f
Rexios <rexios@rexios.dev>
2025-05-15 08:47:19 -0400

    Fix IsolatedHive web init

--- hive_flutter/lib/src/isolated_hive_extensions.dart
+++ hive_flutter/lib/src/isolated_hive_extensions.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive_ce_flutter/adapters.dart'
     hide IsolatedHive, IsolateNameServer;
 import 'package:hive_ce_flutter/src/isolate/isolate_name_server.dart';
 import 'package:hive_ce_flutter/src/wrapper/path_provider.dart';
-import 'package:path/path.dart' as path;
+import 'package:hive_ce_flutter/src/wrapper/path.dart' as path_helper;
 
 /// Flutter extensions for [IsolatedHiveInterface]
 extension IsolatedHiveX on IsolatedHiveInterface {
@@ -20,10 +21,13 @@ extension IsolatedHiveX on IsolatedHiveInterface {
   }) async {
     WidgetsFlutterBinding.ensureInitialized();
 
-    final appDir = await getApplicationDocumentsDirectory();
-    final hivePath = path.join(appDir.path, subDirectory);
+    String? path;
+    if (!kIsWeb) {
+      final appDir = await getApplicationDocumentsDirectory();
+      path = path_helper.join(appDir.path, subDirectory);
+    }
 
-    await init(hivePath, isolateNameServer: const IsolateNameServer());
+    await init(path, isolateNameServer: const IsolateNameServer());
 
     final colorAdapter = ColorAdapter(typeId: colorAdapterTypeId);
     if (!isAdapterRegistered(colorAdapter.typeId)) {
