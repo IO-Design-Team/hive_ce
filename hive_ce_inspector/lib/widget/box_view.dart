4cf0ab0fbb585309917f8289bc0608a371f8137c
Rexios <rexios@rexios.dev>
2025-06-23 22:55:15 -0400

    wip

--- hive_ce_inspector/lib/widget/box_view.dart
+++ hive_ce_inspector/lib/widget/box_view.dart
@@ -13,104 +13,46 @@ class BoxView extends StatefulWidget {
 }
 
 class _BoxViewState extends State<BoxView> {
-  final typeIds = <int>[];
-  int? selectedTypeId;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final typeIds =
-        widget.data.frames.values
-            .map((e) => e.value)
-            .whereType<RawType>()
-            .map((e) => e.typeId)
-            .toSet();
-
-    this.typeIds
-      ..addAll(typeIds)
-      ..sort();
-
-    selectedTypeId = typeIds.firstOrNull;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final List<InspectorFrame> filteredFrames;
-    if (selectedTypeId == null) {
-      filteredFrames = widget.data.frames.values.toList();
-    } else {
-      filteredFrames =
-          widget.data.frames.values.where((e) {
-            final value = e.value;
-            return value is RawType && value.typeId == selectedTypeId;
-          }).toList();
-    }
-
-    if (filteredFrames.isEmpty) {
+    if (widget.data.frames.isEmpty) {
       return const Center(child: Text('Box is empty'));
     }
 
-    final frameFields = fieldsForFrames(filteredFrames);
+    final frames = widget.data.frames.values.toList();
+    final frameFields = fieldsForFrames(frames);
     final columnCount = 1 + (frameFields.values.firstOrNull?.length ?? 0);
 
-    return Column(
-      children: [
-        if (typeIds.length > 1)
-          Row(
-            children: [
-              const Text('Type ID'),
-              const Spacer(),
-              DropdownButton(
-                items: [
-                  for (final typeId in typeIds)
-                    DropdownMenuItem(
-                      value: typeId,
-                      child: Text(typeId.toString()),
-                    ),
-                ],
-                onChanged: (value) => setState(() => selectedTypeId = value),
-              ),
-            ],
-          ),
-        Expanded(
-          child: TableView(
-            delegate: TableCellBuilderDelegate(
-              rowCount: filteredFrames.length + 1,
-              columnCount: columnCount,
-              pinnedRowCount: 1,
-              pinnedColumnCount: 1,
-              rowBuilder:
-                  (index) => const TableSpan(extent: FixedSpanExtent(20)),
-              columnBuilder:
-                  (index) => const TableSpan(extent: FixedSpanExtent(100)),
-              cellBuilder: (context, vicinity) {
-                final TableVicinity(:row, :column) = vicinity;
-                final rowIndex = row - 1;
-                final columnIndex = column - 1;
+    return TableView.builder(
+      rowCount: frames.length + 1,
+      columnCount: columnCount,
+      pinnedRowCount: 1,
+      pinnedColumnCount: 1,
+      rowBuilder: (index) => const TableSpan(extent: FixedSpanExtent(20)),
+      columnBuilder: (index) => const TableSpan(extent: FixedSpanExtent(100)),
+      cellBuilder: (context, vicinity) {
+        final TableVicinity(:row, :column) = vicinity;
+        final rowIndex = row - 1;
+        final columnIndex = column - 1;
 
-                if (row == 0 && column == 0) {
-                  return const TableViewCell(child: Text('Key'));
-                }
+        if (row == 0 && column == 0) {
+          return const TableViewCell(child: Text('Key'));
+        }
 
-                if (row == 0) {
-                  final field = frameFields.values.first[columnIndex];
-                  return TableViewCell(child: Text(field.index.toString()));
-                }
+        if (row == 0) {
+          final field = frameFields.values.first[columnIndex];
+          return TableViewCell(child: Text(field.index.toString()));
+        }
 
-                final frame = filteredFrames.reversed.elementAt(rowIndex);
+        final frame = frames.reversed.elementAt(rowIndex);
 
-                if (column == 0) {
-                  return TableViewCell(child: Text(frame.key.toString()));
-                }
+        if (column == 0) {
+          return TableViewCell(child: Text(frame.key.toString()));
+        }
 
-                final field = frameFields[frame.key]![columnIndex];
-                return TableViewCell(child: Text(field.value.toString()));
-              },
-            ),
-          ),
-        ),
-      ],
+        final field = frameFields[frame.key]![columnIndex];
+        return TableViewCell(child: Text(field.value.toString()));
+      },
     );
   }
 
@@ -124,28 +66,25 @@ class _BoxViewState extends State<BoxView> {
       if (value is RawObject) {
         fields[frame.key] = fieldsForObject(value);
       } else {
-        fields[frame.key] = [IndexedObject(value)];
+        fields[frame.key] = [IndexedObject(0, value)];
       }
     }
 
     return fields;
   }
 
-  List<IndexedObject> fieldsForObject(
-    RawObject object, {
-    List<int>? previousIndex,
-  }) {
+  List<IndexedObject> fieldsForObject(RawObject object) {
     final fields = <IndexedObject>[];
 
     for (final field in object.fields) {
-      final index = [...?previousIndex, field.index];
+      final index = field.index;
       final value = field.value;
       if (value is RawObject) {
         print('INDEX: $index, VALUE: ${value.fields.map((e) => e.value)}');
-        fields.addAll(fieldsForObject(value, previousIndex: index));
+        fields.addAll(fieldsForObject(value));
       } else {
         print('INDEX: $index, VALUE: $value');
-        fields.add(IndexedObject(value, index: index));
+        fields.add(IndexedObject(index, value));
       }
     }
 
@@ -154,8 +93,8 @@ class _BoxViewState extends State<BoxView> {
 }
 
 class IndexedObject {
+  final int index;
   final Object? value;
-  final List<int> index;
 
-  const IndexedObject(this.value, {this.index = const [0]});
+  const IndexedObject(this.index, this.value);
 }
