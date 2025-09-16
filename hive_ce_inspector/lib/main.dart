6bd5f6403c38cb598553d7298693ed310279f71e
Rexios <rexios@rexios.dev>
2025-09-15 22:58:45 -0400

    Cleanup

--- hive_ce_inspector/lib/main.dart
+++ hive_ce_inspector/lib/main.dart
@@ -3,14 +3,5 @@ import 'package:flutter/material.dart';
 import 'package:hive_ce_inspector/widget/connection_screen.dart';
 
 void main() {
-  runApp(
-    DevToolsExtension(
-      child: Navigator(
-        onGenerateRoute: (settings) => MaterialPageRoute(
-          settings: settings,
-          builder: (context) => const ConnectionScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const DevToolsExtension(child: ConnectionScreen()));
 }
