f1a7c5147b68303f424cb9ad7e57518305dfa048
Rexios <rexios@rexios.dev>
2026-01-20 09:08:16 -0500

    Update bundle script

--- hive/bundle.sh
+++ hive/bundle.sh
@@ -6,6 +6,3 @@ flutter build web --wasm
 rm -rf ../hive/extension/devtools/build
 cp -r build/web ../hive/extension/devtools/build
 dart run devtools_extensions validate --package=../hive
-
-cd ../hive
-dart pub publish
